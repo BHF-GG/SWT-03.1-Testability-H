866f2433e55c24fd1aae365ea099551f66f96668
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-09-11 15:04:50 +0200

    Add project files.

---
+++ ECS.H/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.H
+{
+    class Application
+    {public static void Main(string[] args)
+        {
+            var ecs = new ECS(28);
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+        }
+    }
+}
---
+++ ECS.H/TempSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.H
+{
+    class TempSensor
+    {
+        private readonly Random _gen = new Random();
+        public int GetTemp()
+        {
+            return _gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
