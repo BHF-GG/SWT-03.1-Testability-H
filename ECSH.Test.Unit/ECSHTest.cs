47632984c770cdd00e95c1352d97cd2d33ee178c
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-09-25 13:12:27 +0200

    Substitute Test added

--- ECSH.Test.Unit/ECSHTest.cs
+++ ECSH.Test.Unit/ECSHTest.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace ECSH.Test.Unit
@@ -12,9 +13,13 @@ namespace ECSH.Test.Unit
     public class ECSHTest
     {
         private ECSH.ECS uut;
+        private ECSH.ECS uuts;
         private FakeTempSensor fTempSensor;
         private FakeHeater fHeater;
         private FakeWindow fWindow;
+        private ITempSensor sTempSensor;
+        private IHeater sHeater;
+        private IWindow sWindow;
 
         [SetUp]
         public void SetUp()
@@ -23,7 +28,39 @@ namespace ECSH.Test.Unit
             fHeater = new FakeHeater();
             fWindow = new FakeWindow();
             uut = new ECSH.ECS(28, fTempSensor, fHeater, fWindow);
+
+
+            sTempSensor = Substitute.For<ITempSensor>();
+            sHeater = Substitute.For<IHeater>();
+            sWindow = Substitute.For<IWindow>();
+            uuts = new ECSH.ECS(28,sTempSensor,sHeater,sWindow);
         }
+
+        #region SubstituteTests
+
+        [TestCase(5)]
+        [TestCase(-2)]
+        [TestCase(0)]
+        public void GetCurTemp_Returns_Correct(int tempValue)
+        {
+            sTempSensor.GetTemp().Returns(tempValue);
+            Assert.That(uuts.GetCurTemp(), Is.EqualTo(tempValue));
+        }
+
+        [TestCase(1, true, 0)]
+        [TestCase(32, false, 0)]
+        public void Regulate_windowStayClosed(int tempValue, bool heaterRunning, int windowOpened)
+        {
+            sTempSensor.GetTemp().Returns(tempValue);
+            uuts.Regulate();
+            sWindow.Received(windowOpened).Open();
+        }
+
+        #endregion
+
+
+
+        #region FakeTests
 
         [TestCase(10)]
         [TestCase(-5)]
@@ -53,15 +90,18 @@ namespace ECSH.Test.Unit
             Assert.That(fWindow.IsOpen, Is.EqualTo(c));
         }
 
-        [TestCase(true,true,true)]
-        [TestCase(false,true,false)]
-        [TestCase(true,false,false)]
+        [TestCase(true, true, true)]
+        [TestCase(false, true, false)]
+        [TestCase(true, false, false)]
         public void RunSelfTest_Should_Return_C(bool a, bool b, bool c)
         {
             fTempSensor.SelfTest = a;
             fHeater.SelfTest = b;
             Assert.That(uut.RunSelfTest(), Is.EqualTo(c));
         }
+
+        #endregion
+
 
 
     }
